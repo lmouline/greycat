4d962811f3b9e3569dc20ee79265295287e1ceaf
Francois Fouquet <fouquet.f@gmail.com>
2016-08-19 11:21:41 +0200

    try to fix the caching of action

--- core/src/main/java/org/mwg/core/task/ActionPlugin.java
+++ core/src/main/java/org/mwg/core/task/ActionPlugin.java
@@ -11,7 +11,7 @@ class ActionPlugin extends AbstractTaskAction {
 
     private final String _actionName;
     private final String _flatParams;
-    private boolean initilized = false;
+    //private boolean initilized = false;
 
     private TaskAction subAction = null;
 
@@ -25,59 +25,59 @@ class ActionPlugin extends AbstractTaskAction {
     public void eval(final TaskContext context) {
         final String templatedName = context.template(_actionName);
         final String templatedParams = context.template(_flatParams);
-        if (!initilized) {
-            final TaskActionFactory actionFactory = context.graph().taskAction(templatedName);
-            if (actionFactory == null) {
-                throw new RuntimeException("Unknown task action: " + templatedName);
-            }
-            int paramsCapacity = CoreConstants.MAP_INITIAL_CAPACITY;
-            String[] params = new String[paramsCapacity];
-            int paramsIndex = 0;
-            int cursor = 0;
-            int flatSize = templatedParams.length();
-            int previous = 0;
-            while (cursor < flatSize) {
-                char current = templatedParams.charAt(cursor);
-                if (current == Constants.QUERY_SEP) {
-                    String param = templatedParams.substring(previous, cursor);
-                    if (param.length() > 0) {
-                        if (paramsIndex >= paramsCapacity) {
-                            int newParamsCapacity = paramsCapacity * 2;
-                            String[] newParams = new String[newParamsCapacity];
-                            System.arraycopy(params, 0, newParams, 0, paramsCapacity);
-                            params = newParams;
-                            paramsCapacity = newParamsCapacity;
-                        }
-                        params[paramsIndex] = param;
-                        paramsIndex++;
-                    }
-                    previous = cursor + 1;
-                }
-                cursor++;
-            }
-            //add last param
-            String param = templatedParams.substring(previous, cursor);
-            if (param.length() > 0) {
-                if (paramsIndex >= paramsCapacity) {
-                    int newParamsCapacity = paramsCapacity * 2;
-                    String[] newParams = new String[newParamsCapacity];
-                    System.arraycopy(params, 0, newParams, 0, paramsCapacity);
-                    params = newParams;
-                    paramsCapacity = newParamsCapacity;
-                }
-                params[paramsIndex] = param;
-                paramsIndex++;
-            }
-            //schrink
-            if (paramsIndex < params.length) {
-                String[] shrinked = new String[paramsIndex];
-                System.arraycopy(params, 0, shrinked, 0, paramsIndex);
-                params = shrinked;
-            }
-            //add the action to the action
-            subAction = actionFactory.create(params);
-            initilized = true;
+        //if (!initilized) {
+        final TaskActionFactory actionFactory = context.graph().taskAction(templatedName);
+        if (actionFactory == null) {
+            throw new RuntimeException("Unknown task action: " + templatedName);
         }
+        int paramsCapacity = CoreConstants.MAP_INITIAL_CAPACITY;
+        String[] params = new String[paramsCapacity];
+        int paramsIndex = 0;
+        int cursor = 0;
+        int flatSize = templatedParams.length();
+        int previous = 0;
+        while (cursor < flatSize) {
+            char current = templatedParams.charAt(cursor);
+            if (current == Constants.QUERY_SEP) {
+                String param = templatedParams.substring(previous, cursor);
+                if (param.length() > 0) {
+                    if (paramsIndex >= paramsCapacity) {
+                        int newParamsCapacity = paramsCapacity * 2;
+                        String[] newParams = new String[newParamsCapacity];
+                        System.arraycopy(params, 0, newParams, 0, paramsCapacity);
+                        params = newParams;
+                        paramsCapacity = newParamsCapacity;
+                    }
+                    params[paramsIndex] = param;
+                    paramsIndex++;
+                }
+                previous = cursor + 1;
+            }
+            cursor++;
+        }
+        //add last param
+        String param = templatedParams.substring(previous, cursor);
+        if (param.length() > 0) {
+            if (paramsIndex >= paramsCapacity) {
+                int newParamsCapacity = paramsCapacity * 2;
+                String[] newParams = new String[newParamsCapacity];
+                System.arraycopy(params, 0, newParams, 0, paramsCapacity);
+                params = newParams;
+                paramsCapacity = newParamsCapacity;
+            }
+            params[paramsIndex] = param;
+            paramsIndex++;
+        }
+        //schrink
+        if (paramsIndex < params.length) {
+            String[] shrinked = new String[paramsIndex];
+            System.arraycopy(params, 0, shrinked, 0, paramsIndex);
+            params = shrinked;
+        }
+        //add the action to the action
+        subAction = actionFactory.create(params);
+        //initilized = true;
+        // }
         if (subAction != null) {
             subAction.eval(context);
         } else {
