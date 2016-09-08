e244c7e4fe00ebb2b126fe930e0e19aaf17fda9f
Francois Fouquet <fouquet.f@gmail.com>
2016-09-08 14:50:28 +0200

    cleanup structure plugin

---
+++ plugins/structure/src/main/java/org/mwg/structure/StructPlugin.java
@@ -0,0 +1,84 @@
+package org.mwg.structure;
+
+import org.mwg.Graph;
+import org.mwg.Node;
+import org.mwg.plugin.AbstractPlugin;
+import org.mwg.plugin.NodeFactory;
+import org.mwg.structure.action.*;
+import org.mwg.structure.tree.KDTree;
+import org.mwg.task.TaskAction;
+import org.mwg.task.TaskActionFactory;
+
+public class StructPlugin extends AbstractPlugin {
+
+    public StructPlugin() {
+        super();
+        declareNodeType(KDTree.NAME, new NodeFactory() {
+            @Override
+            public Node create(long world, long time, long id, Graph graph) {
+                return new KDTree(world, time, id, graph);
+            }
+        });
+        declareTaskAction(NTreeInsertTo.NAME, new TaskActionFactory() {
+            @Override
+            public TaskAction create(String[] params) {
+                if (params.length != 1) {
+                    throw new RuntimeException("Bad param number!");
+                }
+                return new NTreeInsertTo(params[0]);
+            }
+        });
+        declareTaskAction(TraverseById.NAME, new TaskActionFactory() {
+            @Override
+            public TaskAction create(String[] params) {
+                if (params.length != 1) {
+                    throw new RuntimeException("Bad param number!");
+                }
+                return new TraverseById(params[0]);
+            }
+        });
+        declareTaskAction(NTreeNearestN.NAME, new TaskActionFactory() {
+            @Override
+            public TaskAction create(String[] params) {
+                if (params.length < 2) {
+                    throw new RuntimeException("Bad param number!");
+                }
+                int n = Integer.parseInt(params[params.length - 1]);
+                double[] key = new double[params.length - 1];
+                for (int i = 0; i < params.length - 1; i++) {
+                    key[i] = Double.parseDouble(params[i]);
+                }
+                return new NTreeNearestN(key, n);
+            }
+        });
+        declareTaskAction(NTreeNearestWithinRadius.NAME, new TaskActionFactory() {
+            @Override
+            public TaskAction create(String[] params) {
+                if (params.length < 2) {
+                    throw new RuntimeException("Bad param number!");
+                }
+                double radius = Double.parseDouble(params[params.length - 1]);
+                double[] key = new double[params.length - 1];
+                for (int i = 0; i < params.length - 1; i++) {
+                    key[i] = Double.parseDouble(params[i]);
+                }
+                return new NTreeNearestWithinRadius(key, radius);
+            }
+        });
+        declareTaskAction(NTreeNearestNWithinRadius.NAME, new TaskActionFactory() {
+            @Override
+            public TaskAction create(String[] params) {
+                if (params.length < 3) {
+                    throw new RuntimeException("Bad param number!");
+                }
+                double radius = Double.parseDouble(params[params.length - 1]);
+                int n = Integer.parseInt(params[params.length - 2]);
+                double[] key = new double[params.length - 2];
+                for (int i = 0; i < params.length - 2; i++) {
+                    key[i] = Double.parseDouble(params[i]);
+                }
+                return new NTreeNearestNWithinRadius(key, n, radius);
+            }
+        });
+    }
+}
---
+++ plugins/structure/src/test/java/org/mwg/structure/GeoIndexTaskTest.java
@@ -0,0 +1,111 @@
+package org.mwg.structure;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mwg.Callback;
+import org.mwg.Graph;
+import org.mwg.GraphBuilder;
+import org.mwg.Type;
+import org.mwg.structure.action.NTreeInsertTo;
+import org.mwg.structure.action.NTreeNearestN;
+import org.mwg.structure.action.NTreeNearestNWithinRadius;
+import org.mwg.structure.action.NTreeNearestWithinRadius;
+import org.mwg.structure.distance.Distances;
+import org.mwg.structure.tree.KDTree;
+import org.mwg.task.Action;
+import org.mwg.task.Task;
+import org.mwg.task.TaskContext;
+import org.mwg.task.TaskFunctionConditional;
+
+import static org.mwg.task.Actions.*;
+
+public class GeoIndexTaskTest {
+
+    /**
+     * {@native ts
+     * return inS.indexOf(inS2) !== -1;
+     * }
+     */
+    private static boolean stringContains(String inS, String inS2) {
+        return inS.contains(inS2);
+    }
+
+    @Test
+    public void test() {
+        Graph g = new GraphBuilder().withMemorySize(100000).withPlugin(new StructPlugin()).build();
+        g.connect(new Callback<Boolean>() {
+            @Override
+            public void on(Boolean result) {
+
+                Task createGeoIndex = newTypedNode(KDTree.NAME)
+                        .setProperty(KDTree.DISTANCE, Type.INT, Distances.GEODISTANCE + "")
+                        .setProperty(KDTree.FROM, Type.STRING, "lat,long")
+                        .asGlobalVar("geoIndex");
+
+                Task createTenPoints = loop("0", "9", newNode().setProperty("lat", Type.DOUBLE, "49.{{i}}").setProperty("long", Type.DOUBLE, "6.{{i}}").addToGlobalVar("points"));
+
+                /*
+                double[] y={49.6116,6.1319};
+                double[] x=new double[2];
+                for(int i=0;i<=9;i++){
+                    x[0]=i*0.1+49;
+                    x[1]=i*0.1+6;
+                    System.out.println(GeoDistance.instance().measure(y,x));
+                }
+                System.out.println("Hello");
+*/
+                newTask()
+                        .subTask(createGeoIndex)
+                        .subTask(createTenPoints)
+                        .fromVar("points")
+                        .action(NTreeInsertTo.NAME, "geoIndex")
+                        .fromVar("geoIndex")
+                        .whileDo((new TaskFunctionConditional() {
+                            @Override
+                            public boolean eval(TaskContext context) {
+                                return context.result().size() > 0;
+                            }
+                        }), print("{{result}}").subTasks(new Task[]{traverse("left"), traverse("right")}))
+                        .fromVar("geoIndex")
+                        //.print("{{result}}")
+                        .action(NTreeNearestN.NAME, "49.6116,6.1319,10") //lat,long,nb
+                        //.print("{{result}}")
+                        .then(new Action() {
+                            @Override
+                            public void eval(TaskContext context) {
+                                Assert.assertTrue(stringContains(context.toString(), "{\"world\":0,\"time\":0,\"id\":7,\"lat\":49.5,\"long\":6.5}"));
+                                Assert.assertTrue(stringContains(context.toString(), "{\"world\":0,\"time\":0,\"id\":6,\"lat\":49.4,\"long\":6.4}"));
+                                Assert.assertTrue(stringContains(context.toString(), "{\"world\":0,\"time\":0,\"id\":8,\"lat\":49.6,\"long\":6.6}"));
+                                context.continueTask();
+                            }
+                        })
+                        .fromVar("geoIndex")
+                        .action(NTreeNearestWithinRadius.NAME, "49.6116,6.1319,1000000") //lat,long,meters
+                        .then(new Action() {
+                            @Override
+                            public void eval(TaskContext context) {
+                                Assert.assertTrue(stringContains(context.toString(), "{\"world\":0,\"time\":0,\"id\":7,\"lat\":49.5,\"long\":6.5},{\"world\":0,\"time\":0,\"id\":6,\"lat\":49.4,\"long\":6.4}"));
+                                Assert.assertTrue(stringContains(context.toString(), "{\"world\":0,\"time\":0,\"id\":8,\"lat\":49.6,\"long\":6.6},{\"world\":0,\"time\":0,\"id\":5,\"lat\":49.3,\"long\":6.3}"));
+                                Assert.assertTrue(stringContains(context.toString(), "{\"world\":0,\"time\":0,\"id\":9,\"lat\":49.7,\"long\":6.7},{\"world\":0,\"time\":0,\"id\":4,\"lat\":49.2,\"long\":6.2}"));
+                                Assert.assertTrue(stringContains(context.toString(), "{\"world\":0,\"time\":0,\"id\":10,\"lat\":49.8,\"long\":6.8},{\"world\":0,\"time\":0,\"id\":3,\"lat\":49.1,\"long\":6.1}"));
+                                Assert.assertTrue(stringContains(context.toString(), "{\"world\":0,\"time\":0,\"id\":11,\"lat\":49.9,\"long\":6.9},{\"world\":0,\"time\":0,\"id\":2,\"lat\":49.0,\"long\":6.0}"));
+                                context.continueTask();
+                            }
+                        })
+                        .fromVar("geoIndex")
+                        .action(NTreeNearestNWithinRadius.NAME, "49.6116,6.1319,2,100000") //lat,long,nb,meters
+                        .then(new Action() {
+                            @Override
+                            public void eval(TaskContext context) {
+                                Assert.assertTrue(stringContains(context.toString(), "{\"world\":0,\"time\":0,\"id\":7,\"lat\":49.5,\"long\":6.5}"));
+                                Assert.assertTrue(stringContains(context.toString(), "{\"world\":0,\"time\":0,\"id\":6,\"lat\":49.4,\"long\":6.4}"));
+                                context.continueTask();
+                            }
+                        })
+                        .execute(g, null);
+            }
+        });
+
+    }
+
+}
