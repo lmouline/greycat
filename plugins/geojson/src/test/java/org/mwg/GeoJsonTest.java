6f79b5346dc3ca376c92c2c849bb13e27d2a8f66
Grégory Nain <gregory.nain@gmail.com>
2016-09-09 08:34:51 +0200

    merge

--- plugins/geojson/src/test/java/org/mwg/GeoJsonTest.java
+++ plugins/geojson/src/test/java/org/mwg/GeoJsonTest.java
@@ -2,7 +2,6 @@ package org.mwg;
 
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
-import org.mwg.ml.MLPlugin;
 import org.mwg.plugin.geojson.GeoJsonPlugin;
 import org.mwg.structure.tree.KDTree;
 import org.mwg.task.*;
@@ -18,6 +17,10 @@ import static org.mwg.task.Actions.*;
 
 public class GeoJsonTest {
 
+    //API KEY:          9c84c41dfc20a1539e3cb1239efa65889b6341e4
+    // GetStations:     https://api.jcdecaux.com/vls/v1/stations
+    // Contract information GET https://api.jcdecaux.com/vls/v1/stations/{station_number}?contract={contract_name} HTTP/1.1
+
     private final static String _baseParam = "?apiKey=9c84c41dfc20a1539e3cb1239efa65889b6341e4";
     private final static String _stationsListAddress = "https://api.jcdecaux.com/vls/v1/stations";
     private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
@@ -27,11 +30,7 @@ public class GeoJsonTest {
     public void baseTest() {
 
 
-        //API KEY:          9c84c41dfc20a1539e3cb1239efa65889b6341e4
-        // GetStations:     https://api.jcdecaux.com/vls/v1/stations
-        // Contract information GET https://api.jcdecaux.com/vls/v1/stations/{station_number}?contract={contract_name} HTTP/1.1
-
-        final Graph g = new GraphBuilder().withPlugin(new GeoJsonPlugin()).withPlugin(new MLPlugin()).withPlugin(new VerbosePlugin()).build();
+        final Graph g = new GraphBuilder().withPlugin(new GeoJsonPlugin()).withPlugin(new StructPlugin()).withPlugin(new VerbosePlugin()).build();
         g.connect(connectionResult -> {
 
             WSServer graphServer = new WSServer(g, 8050);
@@ -60,100 +59,16 @@ public class GeoJsonTest {
         });
     }
 
-    private void update(Graph graph) {
 
-        System.out.println("Updating");
-
-
-        final Task update =
-                fromIndexAll("positionsTree")
-                        .hook(new VerboseHookFactory())
-                        .ifThen(new TaskFunctionConditional() {
-                            @Override
-                            public boolean eval(TaskContext context) {
-                                return context.result().size() == 0;
-                            }
-                        }, setTime("" + Constants.BEGINNING_OF_TIME).newTypedNode(KDTree.NAME).print("Positions Tree created !").indexNode("positionsTree", ""))
-                        .asVar("tree")
-                        .action(LOADJSON, _stationsListAddress + _baseParam)
-                        //.hook(new VerboseHookFactory())
-                        .foreach(
-                                setTime(System.currentTimeMillis() + "")
-                                        .asVar("jsonObject")
-                                        .then(new Action() {
-                                            @Override
-                                            public void eval(TaskContext context) {
-                                                JsonObject obj = (JsonObject) context.result().get(0);
-
-                                                Query q = graph.newQuery();
-                                                q.setTime(context.time());
-                                                q.setWorld(context.world());
-                                                q.setIndexName("stations");
-                                                q.add("contract_name", obj.get("contract_name").asString());
-                                                q.add("name", obj.get("name").asString());
-
-                                                graph.findByQuery(q, new Callback<Node[]>() {
-                                                    @Override
-                                                    public void on(Node[] result) {
-                                                        TaskResult<Node> newRes = context.newResult();
-                                                        if (result.length > 0) {
-                                                            newRes.add(result[0]);
-                                                        }
-                                                        context.continueWith(newRes);
-                                                    }
-                                                });
-                                            }
-                                        })
-                                        .ifThen(new TaskFunctionConditional() {
-                                            @Override
-                                            public boolean eval(TaskContext context) {
-                                                return context.result().size() == 0;
-                                            }
-                                        }, fromVar("jsonObject")
-                                                .action(NEW_NODE_FROM_JSON, "")
-                                                .indexNode("stations", "contract_name,name")
-                                                .isolate(asVar("parent").traverse("position").then(new Action() {
-                                                    @Override
-                                                    public void eval(TaskContext context) {
-                                                        KDTree tree = (KDTree) context.variable("tree").get(0);
-                                                        Node n = context.resultAsNodes().get(0);
-                                                        tree.insertWith(
-                                                                new double[]{(double) n.get("lat"), (double) n.get("lng")},
-                                                                (Node) context.variable("parent").get(0),
-                                                                new Callback<Boolean>() {
-                                                                    @Override
-                                                                    public void on(Boolean result) {
-                                                                        context.continueTask();
-                                                                    }
-                                                                });
-                                                    }
-                                                }))
-                                                .print("New Station Created: {{result}}"))
-                                        .then(updateJsonFields)
-                                        .isolate(asVar("parent").traverse("position").then(new Action() {
-                                            @Override
-                                            public void eval(TaskContext context) {
-                                                KDTree tree = (KDTree) context.variable("tree").get(0);
-                                                // tree.set(KDTree.);
-                                                Node n = context.resultAsNodes().get(0);
-                                                tree.insertWith(
-                                                        new double[]{(double) n.get("lat"), (double) n.get("lng")},
-                                                        (Node) context.variable("parent").get(0),
-                                                        new Callback<Boolean>() {
-                                                            @Override
-                                                            public void on(Boolean result) {
-                                                                context.continueTask();
-                                                            }
-                                                        });
-                                            }
-                                        }))
-                                        .clear()
-                        ).print("Update done.")
-                        .clear();
-
-        update.execute(graph, null);
-
-    }
+    final Task getOrCreatePositionsTree =
+            fromIndexAll("positionsTree")
+                    .ifThen(new TaskFunctionConditional() {
+                        @Override
+                        public boolean eval(TaskContext context) {
+                            return context.result().size() == 0;
+                        }
+                    }, setTime("" + Constants.BEGINNING_OF_TIME).newTypedNode(KDTree.NAME).print("Positions Tree created !").indexNode("positionsTree", ""))
+                    .asVar("tree");
 
     private Action updateJsonFields = new Action() {
         @Override
@@ -192,6 +107,96 @@ public class GeoJsonTest {
             context.continueTask();
         }
     };
+
+    final Task updateFromJsonValue =
+            setTime(System.currentTimeMillis() + "")
+                    .asVar("jsonObject")
+                    .then(new Action() {
+                        @Override
+                        public void eval(TaskContext context) {
+                            JsonObject obj = (JsonObject) context.result().get(0);
+
+                            Query q = context.graph().newQuery();
+                            q.setTime(context.time());
+                            q.setWorld(context.world());
+                            q.setIndexName("stations");
+                            q.add("contract_name", obj.get("contract_name").asString());
+                            q.add("name", obj.get("name").asString());
+
+                            context.graph().findByQuery(q, new Callback<Node[]>() {
+                                @Override
+                                public void on(Node[] result) {
+                                    TaskResult<Node> newRes = context.newResult();
+                                    if (result.length > 0) {
+                                        newRes.add(result[0]);
+                                    }
+                                    context.continueWith(newRes);
+                                    context.graph().freeNodes(result);
+                                }
+                            });
+                        }
+                    })
+                    .ifThen(new TaskFunctionConditional() {
+                        @Override
+                        public boolean eval(TaskContext context) {
+                            return context.result().size() == 0;
+                        }
+                    }, fromVar("jsonObject")
+                            .action(NEW_NODE_FROM_JSON, "")
+                            .indexNode("stations", "contract_name,name")
+                            .isolate(asVar("parent").traverse("position").then(new Action() {
+                                @Override
+                                public void eval(TaskContext context) {
+                                    KDTree tree = (KDTree) context.variable("tree").get(0);
+                                    Node n = context.resultAsNodes().get(0);
+                                    tree.insertWith(
+                                            new double[]{(double) n.get("lat"), (double) n.get("lng")},
+                                            (Node) context.variable("parent").get(0),
+                                            new Callback<Boolean>() {
+                                                @Override
+                                                public void on(Boolean result) {
+                                                    context.continueTask();
+                                                }
+                                            });
+                                }
+                            }))
+                            .print("New Station Created: {{result}}"))
+                    .then(updateJsonFields)
+                    .isolate(asVar("parent").traverse("position").then(new Action() {
+                        @Override
+                        public void eval(TaskContext context) {
+                            KDTree tree = (KDTree) context.variable("tree").get(0);
+                            // tree.set(KDTree.);
+                            Node n = context.resultAsNodes().get(0);
+                            tree.insertWith(
+                                    new double[]{(double) n.get("lat"), (double) n.get("lng")},
+                                    (Node) context.variable("parent").get(0),
+                                    new Callback<Boolean>() {
+                                        @Override
+                                        public void on(Boolean result) {
+                                            context.continueTask();
+                                        }
+                                    });
+                        }
+                    }))
+                    .clear();
+
+    final Task update =
+            hook(new VerboseHookFactory())
+                    .subTask(getOrCreatePositionsTree)
+                    .action(LOADJSON, _stationsListAddress + _baseParam)
+                    .foreach(updateFromJsonValue)
+                    .print("Update done.")
+                    .clear();
+
+    private void update(Graph graph) {
+
+        System.out.println("Updating");
+        update.execute(graph, null);
+
+    }
+
+
 
 
     public static void main(String[] args) {
