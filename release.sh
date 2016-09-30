9b8420315eee73aa6668e88138a63f36e6f933c0
Francois Fouquet <fouquet.f@gmail.com>
2016-09-30 16:06:28 +0200

    change

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export JAVA_HOME=$(/usr/libexec/java_home)
-mvn -P release release:clean
-mvn -P release release:prepare
-mvn -P release release:perform
+mvn release:clean
+mvn release:prepare
+mvn release:perform
