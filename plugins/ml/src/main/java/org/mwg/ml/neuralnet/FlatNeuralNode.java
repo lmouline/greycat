bfe623e3fde326e7010afcb1423db4d34b4230e7
Francois Fouquet <fouquet.f@gmail.com>
2016-11-10 14:17:07 +0100

    push

--- plugins/ml/src/main/java/org/mwg/ml/neuralnet/FlatNeuralNode.java
+++ plugins/ml/src/main/java/org/mwg/ml/neuralnet/FlatNeuralNode.java
@@ -1,7 +1,19 @@
 package org.mwg.ml.neuralnet;
 
-/**
- * Created by duke on 10/11/2016.
- */
-public class FlatNeuralNode {
+import org.mwg.Graph;
+import org.mwg.plugin.AbstractNode;
+
+public class FlatNeuralNode extends AbstractNode {
+
+    public FlatNeuralNode(long p_world, long p_time, long p_id, Graph p_graph) {
+        super(p_world, p_time, p_id, p_graph);
+    }
+
+    @Override
+    public void init() {
+        //here come the configuration after creation
+    }
+
+    //TODO
+
 }
