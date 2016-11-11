e5cba0848516993f35bd8f5295e1568e1d48a382
Francois Fouquet <fouquet.f@gmail.com>
2016-11-11 13:49:38 +0100

    cleanup useless JS

--- plugins/ml/src/main/java/org/mwg/ml/common/matrix/DefaultMatrixEngine.java
+++ plugins/ml/src/main/java/org/mwg/ml/common/matrix/DefaultMatrixEngine.java
@@ -3,14 +3,7 @@ package org.mwg.ml.common.matrix;
 public class DefaultMatrixEngine {
 
     private static MatrixEngine _defaultEngine = null;
-
-    /**
-     * @native ts
-     * if(DefaultMatrixEngine._defaultEngine == null){
-     * DefaultMatrixEngine._defaultEngine = new org.mwg.ml.common.matrix.HybridMatrixEngine();
-     * }
-     * return DefaultMatrixEngine._defaultEngine;
-     */
+    
     public static MatrixEngine defaultEngine() {
         if (_defaultEngine == null) {
             _defaultEngine = new HybridMatrixEngine();
