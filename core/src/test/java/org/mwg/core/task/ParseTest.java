f4af31c4c0924a1cdaf13f5222407754a9a58b2f
Francois Fouquet <fouquet.f@gmail.com>
2016-12-09 12:29:40 +0100

    fix

--- core/src/test/java/org/mwg/core/task/ParseTest.java
+++ core/src/test/java/org/mwg/core/task/ParseTest.java
@@ -9,8 +9,7 @@ public class ParseTest extends AbstractActionTest {
     @Test
     public void test() {
         initGraph();
-        newTask().parse("travelInTime(0).travelInWorld(0).createNode().addToVar(root).createNode().addToVar(root).createNode().addToVar(root).readVar(root).println('{{result}} avec echap \\'')", graph)
-                .execute(graph, null);
+        newTask().parse("travelInTime(0).travelInWorld(0).createNode().addToVar(root).createNode().addToVar(root).createNode().addToVar(root).readVar(root).println('{{result}} avec echap \\'')", graph).execute(graph, null);
         removeGraph();
     }
 
