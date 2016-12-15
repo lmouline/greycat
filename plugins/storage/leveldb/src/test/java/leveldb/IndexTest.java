22bbbf57cbb256569cd37d4b3f184c7732271242
Ludovic Mouline <ludovic.mouline@uni.lu>
2016-12-15 14:54:52 +0100

    make the test failed again :)

--- plugins/storage/leveldb/src/test/java/leveldb/IndexTest.java
+++ plugins/storage/leveldb/src/test/java/leveldb/IndexTest.java
@@ -1,14 +1,8 @@
 package leveldb;
 
 import org.junit.Test;
-import org.mwg.Callback;
-import org.mwg.Graph;
-import org.mwg.GraphBuilder;
-import org.mwg.LevelDBStorage;
+import org.mwg.*;
 import org.mwg.core.scheduler.NoopScheduler;
-import org.mwg.core.task.Actions;
-import org.mwg.task.Task;
-import org.mwg.task.TaskResult;
 
 import java.io.IOException;
 import java.nio.file.*;
@@ -21,33 +15,44 @@ public class IndexTest {
         Graph graph = new GraphBuilder().withScheduler(new NoopScheduler()).withStorage(new LevelDBStorage("db")).build();
         Graph graph2 = new GraphBuilder().withStorage(new LevelDBStorage("db")).build();
 
-        final Task task = Actions.newTask()
-                .travelInWorld("0")
-                .travelInTime(System.currentTimeMillis() + "")
-                .readGlobalIndex("indexName")
-                .save();
-
+        final String index = "indexName";
         graph.connect(new Callback<Boolean>() {
             @Override
             public void on(Boolean result) {
-                task.execute(graph, new Callback<TaskResult>() {
-                    @Override
-                    public void on(TaskResult result) {
-                        graph.disconnect(new Callback<Boolean>() {
-                            @Override
-                            public void on(Boolean result) {
-                                graph2.connect(new Callback<Boolean>() {
-                                    @Override
-                                    public void on(Boolean result) {
-                                        task.execute(graph2, null);
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
+               graph.index(0, 0, index, new Callback<NodeIndex>() {
+                   @Override
+                   public void on(NodeIndex result) {
+                       graph.save(new Callback<Boolean>() {
+                           @Override
+                           public void on(Boolean result) {
+                               graph.disconnect(new Callback<Boolean>() {
+                                   @Override
+                                   public void on(Boolean result) {
+                                       graph2.connect(new Callback<Boolean>() {
+                                           @Override
+                                           public void on(Boolean result) {
+                                               graph2.index(0, 0, index, new Callback<NodeIndex>() {
+                                                   @Override
+                                                   public void on(NodeIndex result) {
+                                                        result.find(new Callback<Node[]>() {
+                                                            @Override
+                                                            public void on(Node[] result) {
+
+                                                            }
+                                                        });
+                                                   }
+                                               });
+                                           }
+                                       });
+                                   }
+                               });
+                           }
+                       });
+                   }
+               });
             }
         });
+
 
         try {
             Path path = Paths.get("db");
--- plugins/storage/rocksdb/src/test/java/rocksdb/IndexTest.java
+++ plugins/storage/rocksdb/src/test/java/rocksdb/IndexTest.java
@@ -1,15 +1,12 @@
 package rocksdb;
 
-import org.junit.Assert;
 import org.junit.Test;
-import org.mwg.Callback;
-import org.mwg.Graph;
-import org.mwg.GraphBuilder;
-import org.mwg.RocksDBStorage;
-import org.mwg.core.task.Actions;
-import org.mwg.task.Task;
+import org.mwg.*;
+import org.mwg.core.scheduler.NoopScheduler;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 /**
  * Created by ludovicmouline on 15/12/2016.
@@ -18,47 +15,67 @@ public class IndexTest {
 
     @Test
     public void indexWithDB() {
-        //Bug: Only with storage, if an empty index is only read (without modification), saved,
-        // and read again after a connection, a NullPointerException is thrown
+        Graph graph = new GraphBuilder().withScheduler(new NoopScheduler()).withStorage(new RocksDBStorage("db")).build();
+        Graph graph2 = new GraphBuilder().withStorage(new RocksDBStorage("db")).build();
 
-        final Task readIdx = Actions.newTask()
-                .travelInTime(System.currentTimeMillis() + "")
-                .travelInWorld("0")
-                .readGlobalIndex("index","idx_id","complexOne_complexTwo")
-                .save();
+        final String index = "indexName";
+        graph.connect(new Callback<Boolean>() {
+            @Override
+            public void on(Boolean result) {
+                graph.index(0, 0, index, new Callback<NodeIndex>() {
+                    @Override
+                    public void on(NodeIndex result) {
+                        graph.save(new Callback<Boolean>() {
+                            @Override
+                            public void on(Boolean result) {
+                                graph.disconnect(new Callback<Boolean>() {
+                                    @Override
+                                    public void on(Boolean result) {
+                                        graph2.connect(new Callback<Boolean>() {
+                                            @Override
+                                            public void on(Boolean result) {
+                                                graph2.index(0, 0, index, new Callback<NodeIndex>() {
+                                                    @Override
+                                                    public void on(NodeIndex result) {
+                                                        result.find(new Callback<Node[]>() {
+                                                            @Override
+                                                            public void on(Node[] result) {
 
-        String url = "dbStorage";
-        File location = new File(url);
-        location.deleteOnExit();
-        if (!location.exists()) {
-            location.mkdirs();
-        }
-
-
-        boolean exception = false;
-        Graph graph = new GraphBuilder().withStorage(new RocksDBStorage("dbStorage")).build();
-        try {
-
-            graph.connect(new Callback<Boolean>() {
-                @Override
-                public void on(Boolean result) {
-
-                    readIdx.executeSync(graph);
-
-                    graph.disconnect(succeed -> {
-                        graph.connect(ok -> {
-                            readIdx.executeSync(graph);
+                                                            }
+                                                        });
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
+                                });
+                            }
                         });
-                    });
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-            exception = true;
-        } finally {
-            graph.disconnect(null);
-        }
+                    }
+                });
+            }
+        });
 
-        Assert.assertEquals(false,exception);
+
+        try {
+            Path path = Paths.get("db");
+            if (Files.exists(path)) {
+                Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                        Files.delete(file);
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+            }
+        } catch (Exception e) {
+
+        }
     }
 }
