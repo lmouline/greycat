526357ce09f2e89a53c9b94125422a7ed704622e
Francois Fouquet <fouquet.f@gmail.com>
2016-12-21 15:06:12 +0100

    desactivate boundaries check while not developing OffHeap features

--- plugins/memory/src/main/java/org/mwg/memory/offheap/OffHeapConstants.java
+++ plugins/memory/src/main/java/org/mwg/memory/offheap/OffHeapConstants.java
@@ -8,7 +8,7 @@ public class OffHeapConstants {
     public static final int OFFHEAP_NULL_PTR = -1;
 
     //TODO remove this before release
-    public static boolean DEBUG_MODE = true;
+    public static boolean DEBUG_MODE = false;
     public static final Map<Long, Long> SEGMENTS = new HashMap<Long, Long>();
 
 }
