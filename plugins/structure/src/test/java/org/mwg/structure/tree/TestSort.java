e95d8f4b8425961ac599f679f6bff790dcd5d5f9
Assaad MOAWAD <assaad.moawad@uni.lu>
2017-01-07 17:40:03 +0100

    fix compile

--- plugins/structure/src/test/java/org/mwg/structure/tree/TestSort.java
+++ plugins/structure/src/test/java/org/mwg/structure/tree/TestSort.java
@@ -14,7 +14,7 @@ import java.util.Random;
 public class TestSort {
 
     @Test
-    public void testsort(){
+    public void testsort() {
         final Graph graph = new GraphBuilder()
                 .withPlugin(new StructurePlugin())
                 .withMemorySize(10000)
@@ -23,30 +23,31 @@ public class TestSort {
             @Override
             public void on(Boolean result) {
                 int max = 20;
-                int maxinsert=200;
-                Random random = new Random(1234);
+                int maxinsert = 200;
+                Random random = new Random();
+                random.setSeed(1234);
 
 
                 for (int i = 0; i < max; i++) {
-                    int capacity = i+50;
+                    int capacity = i + 50;
                     Node kmf = graph.newNode(0, 0);
                     EGraph egraph = (EGraph) kmf.getOrCreate("test", Type.EGRAPH);
                     ENode root = egraph.newNode();
                     egraph.setRoot(root);
                     VolatileResult vr = new VolatileResult(root, capacity, -1);
 
-                    for(int k=0;k<maxinsert;k++){
-                        vr.insert(new double[]{k},k,random.nextDouble());
+                    for (int k = 0; k < maxinsert; k++) {
+                        vr.insert(new double[]{k}, k, random.nextDouble());
                     }
                     vr.sort(true);
 
-                    for(int k=0;k<vr.size()-1;k++){
-                        Assert.assertTrue(vr.distance(k)<vr.distance(k+1));
+                    for (int k = 0; k < vr.size() - 1; k++) {
+                        assert (vr.distance(k) < vr.distance(k + 1));
                     }
 
                     vr.sort(false);
-                    for(int k=0;k<vr.size()-1;k++){
-                        Assert.assertTrue(vr.distance(k)>vr.distance(k+1));
+                    for (int k = 0; k < vr.size() - 1; k++) {
+                        assert (vr.distance(k) > vr.distance(k + 1));
                     }
                     kmf.free();
                     graph.save(null);
@@ -54,7 +55,6 @@ public class TestSort {
 
             }
         });
-
 
 
     }
