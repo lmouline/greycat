41fdba8d790aaa8371bee76041ad1d018b87c8c7
Grégory Nain <gregory.nain@gmail.com>
2017-01-12 08:52:36 +0100

    Integrating NDTree in action

--- plugins/structure/src/main/java/org/mwg/structure/action/NTreeNearestNWithinRadius.java
+++ plugins/structure/src/main/java/org/mwg/structure/action/NTreeNearestNWithinRadius.java
@@ -4,7 +4,9 @@ import org.mwg.Callback;
 import org.mwg.DeferCounter;
 import org.mwg.Node;
 import org.mwg.plugin.Job;
+import org.mwg.structure.TreeResult;
 import org.mwg.structure.tree.KDTree;
+import org.mwg.structure.tree.NDTree;
 import org.mwg.task.*;
 
 public class NTreeNearestNWithinRadius implements Action {
@@ -24,13 +26,13 @@ public class NTreeNearestNWithinRadius implements Action {
     @Override
     public final void eval(final TaskContext ctx) {
         final TaskResult previousResult = ctx.result();
-        final TaskResult<Node> nextResult = ctx.newResult();
+        final TaskResult<Object> nextResult = ctx.newResult();
         if (previousResult != null) {
             final DeferCounter defer = ctx.graph().newCounter(previousResult.size());
             final TaskResultIterator previousResultIt = previousResult.iterator();
             Object iter = previousResultIt.next();
             while (iter != null) {
-                if (iter instanceof KDTree ) {
+                if (iter instanceof KDTree) {
                     ((KDTree) iter).nearestNWithinRadius(_key, _n, _radius, new Callback<Node[]>() {
                         @Override
                         public void on(Node[] result) {
@@ -42,6 +44,26 @@ public class NTreeNearestNWithinRadius implements Action {
                             defer.count();
                         }
                     });
+                } else if (iter instanceof NDTree) {
+                    TreeResult tr = ((NDTree) iter).nearestNWithinRadius(_key, _n, _radius);
+
+                    long[] nodeIds = new long[tr.size()];
+                    for (int i = 0; i < tr.size(); i++) {
+                        nodeIds[i] = tr.value(i);
+                    }
+
+                    ctx.graph().lookupAll(ctx.world(), ctx.time(), nodeIds, result -> {
+                        for(int j = 0; j < result.length; j++) {
+                            TaskResult<Object> line = ctx.newResult();
+                            line.add(tr.keys(j));
+                            line.add(result[j]);
+                            line.add(tr.distance(j));
+                            nextResult.add(line);
+                        }
+                        tr.free();
+                        defer.count();
+                    });
+
                 } else {
                     defer.count();
                 }
