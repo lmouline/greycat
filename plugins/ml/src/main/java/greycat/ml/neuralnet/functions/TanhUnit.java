c95d7f73509349174cf78616d328a7686d5120af
Francois Fouquet <fouquet.f@gmail.com>
2017-02-09 15:31:20 +0100

    pushed back

--- plugins/ml/src/main/java/greycat/ml/neuralnet/functions/TanhUnit.java
+++ plugins/ml/src/main/java/greycat/ml/neuralnet/functions/TanhUnit.java
@@ -20,25 +20,42 @@ import greycat.ml.neuralnet.NeuralUnit;
 public class TanhUnit implements NeuralUnit {
 
 
-	private static TanhUnit static_unit= null;
+    private static TanhUnit static_unit = null;
 
-	public static TanhUnit instance() {
-		if (static_unit == null) {
-			static_unit = new TanhUnit();
-		}
-		return static_unit;
-	}
+    public static TanhUnit instance() {
+        if (static_unit == null) {
+            static_unit = new TanhUnit();
+        }
+        return static_unit;
+    }
 
-	@Override
-	public double forward(double x) {
-		return Math.tanh(x);
-	}
+    /**
+     * {@native ts
+     * if (x === Infinity) {return 1;}
+     * if (x === -Infinity) {return -1;}
+     * let y = Math.exp(x * 2);
+     * return (y - 1) / (y + 1);
+     * }
+     */
+    @Override
+    public double forward(double x) {
+        return Math.tanh(x);
+    }
 
-	@Override
-	public double backward(double x) {
-		double coshx = Math.cosh(x);
-		double denom = (Math.cosh(2*x) + 1);
-		return 4 * coshx * coshx / (denom * denom);
-	}
+    @Override
+    public double backward(double x) {
+        double coshx = cosh(x);
+        double denom = (cosh(2 * x) + 1);
+        return 4 * coshx * coshx / (denom * denom);
+    }
+
+    /**
+     * {@native ts
+     * return (Math.exp(x) + Math.exp(-x)) / 2;
+     * }
+     */
+    private static double cosh(double x) {
+        return Math.cosh(x);
+    }
 
 }
