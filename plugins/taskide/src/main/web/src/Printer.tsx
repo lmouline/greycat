548c26d97d29eccc41e851980e62938c63d82809
Francois Fouquet <fouquet.f@gmail.com>
2017-02-17 10:06:03 +0100

    demonstrate TypeScript GreyCat capability improvement for #4

--- plugins/taskide/src/main/web/src/Printer.tsx
+++ plugins/taskide/src/main/web/src/Printer.tsx
@@ -1,10 +1,10 @@
-import * as greycat from 'greycat';
+import {Type} from 'greycat';
 import * as React from 'react';
 
 export class Printer extends React.Component<String,String> {
 
     sayHello() {
-        console.log("hello", greycat.Type.STRING, React);
+        console.log("hello", Type.BOOL, React);
     }
 
 }
