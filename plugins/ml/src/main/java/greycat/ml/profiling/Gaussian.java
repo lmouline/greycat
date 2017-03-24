b5f2af88482c7eea202b1d1452dedeb8b898f8e6
Assaad MOAWAD <assaad.moawad@uni.lu>
2017-03-24 17:58:25 +0100

    histogram and profile

--- plugins/ml/src/main/java/greycat/ml/profiling/Gaussian.java
+++ plugins/ml/src/main/java/greycat/ml/profiling/Gaussian.java
@@ -18,6 +18,7 @@ package greycat.ml.profiling;
 import greycat.Node;
 import greycat.Type;
 import greycat.ml.math.Gaussian1D;
+import greycat.struct.DoubleArray;
 
 /**
  * Created by assaad on 20/02/2017.
@@ -33,6 +34,12 @@ public class Gaussian {
     public static final String TOTAL = "profile_total";
     public static final String PRECISIONS = "profile_precisions"; //Default covariance matrix for a dirac function
     public static final String VALUES = "profile_values";
+
+    public static final String HISTOGRAM_BUCKETS="hist_buckets";
+    public static final int HISTOGRAM_BUCKETS_DEF=20;
+    public static final String HISTOGRAM_MIN="histogram_min";
+    public static final String HISTOGRAM_MAX="histogram_max";
+    public static final String HISTOGRAM_VALUES="histogram_values";
 
     public static void profile(Node host, double value) {
         Double min = host.getWithDefault(MIN, null);
@@ -59,6 +66,47 @@ public class Gaussian {
             double cov = Gaussian1D.getCovariance(sum, sumsq, total);
             host.set(COV, Type.DOUBLE, cov);
             host.set(STD, Type.DOUBLE, Math.sqrt(cov));
+        }
+    }
+
+    public static void clearProfile(Node host) {
+        host.set(TOTAL, Type.LONG, null);
+        host.set(SUM, Type.DOUBLE, null);
+        host.set(SUMSQ, Type.DOUBLE, null);
+        host.set(AVG, Type.DOUBLE, null);
+        host.set(COV, Type.DOUBLE, null);
+        host.set(STD, Type.DOUBLE, null);
+        host.set(HISTOGRAM_MIN,Type.DOUBLE_ARRAY,null);
+        host.set(HISTOGRAM_MAX,Type.DOUBLE_ARRAY,null);
+        host.set(HISTOGRAM_VALUES,Type.DOUBLE_ARRAY,null);
+    }
+
+    public static void histogram(Node host, double min, double max, double value) {
+        if(max<min || value<min || value>max){
+            throw new RuntimeException("value is outside min, max range");
+        }
+        if (max != min) {
+            int steps= host.getWithDefault(HISTOGRAM_BUCKETS,HISTOGRAM_BUCKETS_DEF);
+            double stepsize = (max - min) / steps;
+            DoubleArray hist_min= (DoubleArray) host.getOrCreate(HISTOGRAM_MIN,Type.DOUBLE_ARRAY);
+            DoubleArray hist_max= (DoubleArray) host.getOrCreate(HISTOGRAM_MAX,Type.DOUBLE_ARRAY);
+            DoubleArray hist_values= (DoubleArray) host.getOrCreate(HISTOGRAM_VALUES,Type.DOUBLE_ARRAY);
+
+            if(hist_min.size()==0){
+                hist_min.init(steps);
+                hist_max.init(steps);
+                hist_values.init(steps);
+                for (int i = 0; i < steps; i++) {
+                    hist_min.set(i, min + stepsize * i);
+                    hist_max.set(i, min + stepsize * (i + 1));
+                }
+            }
+
+            int index = (int) ((value - min) / stepsize);
+            if (index == steps) {
+                index--;
+            }
+            hist_values.set(index,hist_values.get(index)+1);
         }
     }
 
