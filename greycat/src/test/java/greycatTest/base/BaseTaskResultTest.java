ec45763e0c4bc5b50f1c6036960b86d96ff58f68
Grégory Nain <gregory.nain@gmail.com>
2017-04-16 12:08:41 +0200

    build fix

--- greycat/src/test/java/greycatTest/base/BaseTaskResultTest.java
+++ greycat/src/test/java/greycatTest/base/BaseTaskResultTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2017 The GreyCat Authors.  All rights reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package greycatTest.base;
 
 import greycat.Graph;
@@ -15,63 +30,25 @@ public class BaseTaskResultTest {
 
     @Test
     public void saveLoadTest() {
-
         Graph graph = GraphBuilder.newBuilder().build();
         graph.connect(connected -> {
-            BaseTaskResult<String> taskResultOrigin = new BaseTaskResult("ResultContent", false);
-            taskResultOrigin.setException(new RuntimeException("Exception text"));
+            try {
+                BaseTaskResult<String> taskResultOrigin = new BaseTaskResult("ResultContent", false);
+                taskResultOrigin.setException(new RuntimeException("Exception text"));
 
-            Buffer buffer = new HeapBuffer();
-            taskResultOrigin.saveToBuffer(buffer);
+                Buffer buffer = new HeapBuffer();
+                taskResultOrigin.saveToBuffer(buffer);
 
-            BaseTaskResult taskResultLoaded = new BaseTaskResult(null, false);
-            taskResultLoaded.load(buffer, graph);
-            Assert.assertEquals(expectedException, taskResultLoaded.exception().toString());
+                BaseTaskResult taskResultLoaded = new BaseTaskResult(null, false);
+                taskResultLoaded.load(buffer, graph);
+
+            } catch (Throwable t) {
+                Assert.fail("Exception raised !");
+                t.printStackTrace();
+            }
             graph.disconnect(null);
         });
     }
-
-    private static final String expectedException="java.lang.Exception: java.lang.RuntimeException: Exception text\n" +
-            "\tat greycatTest.base.BaseTaskResultTest.lambda$saveLoadTest$0(BaseTaskResultTest.java:22)\n" +
-            "\tat greycatTest.base.BaseTaskResultTest$$Lambda$1/824009085.on(Unknown Source)\n" +
-            "\tat greycat.internal.CoreGraph$4$1$1.on(CoreGraph.java:348)\n" +
-            "\tat greycat.internal.CoreGraph$4$1$1.on(CoreGraph.java:309)\n" +
-            "\tat greycat.internal.BlackHoleStorage.get(BlackHoleStorage.java:46)\n" +
-            "\tat greycat.internal.CoreGraph$4$1.on(CoreGraph.java:309)\n" +
-            "\tat greycat.internal.CoreGraph$4$1.on(CoreGraph.java:291)\n" +
-            "\tat greycat.internal.BlackHoleStorage.lock(BlackHoleStorage.java:80)\n" +
-            "\tat greycat.internal.CoreGraph$4.on(CoreGraph.java:291)\n" +
-            "\tat greycat.internal.CoreGraph$4.on(CoreGraph.java:288)\n" +
-            "\tat greycat.internal.BlackHoleStorage.connect(BlackHoleStorage.java:72)\n" +
-            "\tat greycat.internal.CoreGraph.connect(CoreGraph.java:288)\n" +
-            "\tat greycatTest.base.BaseTaskResultTest.saveLoadTest(BaseTaskResultTest.java:20)\n" +
-            "\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n" +
-            "\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\n" +
-            "\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\n" +
-            "\tat java.lang.reflect.Method.invoke(Method.java:497)\n" +
-            "\tat org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:50)\n" +
-            "\tat org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)\n" +
-            "\tat org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:47)\n" +
-            "\tat org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)\n" +
-            "\tat org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:325)\n" +
-            "\tat org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:78)\n" +
-            "\tat org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:57)\n" +
-            "\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)\n" +
-            "\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)\n" +
-            "\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)\n" +
-            "\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)\n" +
-            "\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)\n" +
-            "\tat org.junit.runners.ParentRunner.run(ParentRunner.java:363)\n" +
-            "\tat org.junit.runner.JUnitCore.run(JUnitCore.java:137)\n" +
-            "\tat com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:68)\n" +
-            "\tat com.intellij.rt.execution.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:51)\n" +
-            "\tat com.intellij.rt.execution.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:237)\n" +
-            "\tat com.intellij.rt.execution.junit.JUnitStarter.main(JUnitStarter.java:70)\n" +
-            "\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n" +
-            "\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\n" +
-            "\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\n" +
-            "\tat java.lang.reflect.Method.invoke(Method.java:497)\n" +
-            "\tat com.intellij.rt.execution.application.AppMain.main(AppMain.java:147)\n";
 
 
 }
