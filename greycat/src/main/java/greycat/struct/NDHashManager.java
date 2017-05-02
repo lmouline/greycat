318861a315c9771a3ffc644c62104bc77fd3c156
Assaad MOAWAD <assaad.moawad@uni.lu>
2017-05-02 17:27:37 +0200

    Push first modif

--- greycat/src/main/java/greycat/struct/NDHashManager.java
+++ greycat/src/main/java/greycat/struct/NDHashManager.java
@@ -3,5 +3,30 @@ package greycat.struct;
 /**
  * Created by assaad on 02/05/2017.
  */
-public interface NDManager {
+public interface NDHashManager {
+
+    /**
+     * Create a new element in the hashtable
+     **/
+    long createNewElement();
+
+
+    /**
+     * Update the element in the hashtable
+     *
+     * @param id                the id of the resolved element from the hashtable
+     * @param key               the multi dimensional key being inserted
+     * @param value             the object being indexed/profiled for this key
+     */
+    void update(long id, double[] key, long value);
+
+
+    /**
+     * Resolve the current object of the hashed id 
+     * @param id
+     * @return
+     */
+    Object get(long id);
+
+
 }
