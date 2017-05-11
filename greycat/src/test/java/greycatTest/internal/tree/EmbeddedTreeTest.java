d45420252aa5656e45704eb70f7170c8b0639473
Assaad MOAWAD <assaad.moawad@uni.lu>
2017-05-11 12:01:30 +0200

    First push on

--- greycat/src/test/java/greycatTest/internal/tree/EmbeddedTreeTest.java
+++ greycat/src/test/java/greycatTest/internal/tree/EmbeddedTreeTest.java
@@ -16,6 +16,7 @@
 package greycatTest.internal.tree;
 
 import greycat.*;
+import greycat.internal.tree.NDTree;
 import greycat.scheduler.NoopScheduler;
 import greycat.struct.Profile;
 import greycat.struct.TreeResult;
@@ -34,24 +35,24 @@ public class EmbeddedTreeTest {
             @Override
             public void on(Boolean result) {
                 Node node = g.newNode(0, 0);
-                Profile profile = (Profile) node.getOrCreate("mindex", Type.NDTREE);
+                NDTree profile = (NDTree) node.getOrCreate("mindex", Type.NDTREE);
                 //configure the profile
                 profile.setMinBound(new double[]{0, 0});
                 profile.setMaxBound(new double[]{23, 1000});
                 profile.setResolution(new double[]{1, 100});
                 //profile 550w/h at noon
-                profile.profile(new double[]{12, 550});
+                profile.insert(new double[]{12, 550},1);
                 //profile 450w/h at noon
-                profile.profile(new double[]{12, 450});
+                profile.insert(new double[]{12, 450},1);
                 //profile 800w/h at 4.PM
-                profile.profile(new double[]{16, 800});
+                profile.insert(new double[]{16, 800},1);
 
-                TreeResult treeResult = profile.queryAround(new double[]{12, 500}, 1);
+                TreeResult treeResult = profile.queryAround(new double[]{12, 520}, 1);
 
                 Assert.assertEquals(1, treeResult.size());
                 double[] retrieveKeys = treeResult.keys(0);
                 Assert.assertTrue(12 == retrieveKeys[0]);
-                Assert.assertTrue(500 == retrieveKeys[1]);
+                Assert.assertTrue(550 == retrieveKeys[1]);
                 treeResult.free();
                 
                 g.disconnect(null);
