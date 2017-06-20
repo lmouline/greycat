c186e41b8fa940c3d120894a962a52becbf10a87
Assaad MOAWAD <assaad.moawad@uni.lu>
2017-06-20 14:32:18 +0200

    Finite DIff

--- plugins/ml/src/main/java/greycat/ml/math/timeseries/Stationary.java
+++ plugins/ml/src/main/java/greycat/ml/math/timeseries/Stationary.java
@@ -18,8 +18,41 @@ package greycat.ml.math.timeseries;
 /**
  * Created by assaad on 20/06/2017.
  */
-public class StationaryCalc {
-    public static double[] diff(double[] series) {
+public class Stationary {
+
+
+    public static double autoCorrelation(double[] series, int stepBack) {
+
+        if(stepBack>series.length){
+            throw new RuntimeException("Step back can be maximum equal to array length!");
+        }
+
+        double xy = 0, x = 0, x2 = 0, y = 0, y2 = 0;
+
+        for (int i = stepBack; i < series.length; i++) {
+            xy += series[i] * series[i-stepBack];
+            x += series[i];
+            y += series[i-stepBack];
+            x2 += series[i] * series[i];
+            y2 += series[i-stepBack] * series[i-stepBack];
+        }
+        int n = series.length-stepBack;
+        return (xy - (x * y) / n) / Math.sqrt((x2 - (x * x) / n) * (y2 - (y * y) / n));
+    }
+
+
+    public static double[] autoCorrelationFunction(double[] series, int elements){
+        double[] res=new double[elements];
+
+        for(int i=0;i<elements;i++){
+            res[i]=autoCorrelation(series,i);
+        }
+        return res;
+    }
+
+
+
+    public static double[] difference(double[] series) {
         if (series.length > 1) {
             double[] temp = new double[series.length - 1];
 
@@ -32,17 +65,16 @@ public class StationaryCalc {
         throw new RuntimeException("Series should be at least 2 elements!");
     }
 
-    public static double[] diffNOrder(double[] series, int n) {
+    public static double[] differenceNOrder(double[] series, int n) {
         if (series.length > n) {
             double[] temp = series;
             for (int i = 0; i < n; i++) {
-                temp = diff(temp);
+                temp = difference(temp);
             }
             return temp;
         }
         throw new RuntimeException("Series should be at least N elements!");
     }
-
 
 
 
