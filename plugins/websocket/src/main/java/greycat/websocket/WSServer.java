684cf3171838990453297ddac1cd1c6d247d8cde
Grégory Nain <gregory.nain@gmail.com>
2017-07-19 10:23:26 +0200

    Change todo in noop. Nothing to do server-side if not secured. But method still necessary for Security layer

--- plugins/websocket/src/main/java/greycat/websocket/WSServer.java
+++ plugins/websocket/src/main/java/greycat/websocket/WSServer.java
@@ -95,7 +95,7 @@ public class WSServer implements WebSocketConnectionCallback, Callback<Buffer> {
 
 
     protected void onChannelClosed(WebSocketChannel webSocketChannel, StreamSourceFrameChannel channel) {
-        //TODO: notify Graph !
+        //NOOP
     }
 
     @Override
