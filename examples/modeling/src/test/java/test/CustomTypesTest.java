6e90e5f0b1e33a76b4e4501fdf8136d76b3db6e3
Francois Fouquet <fouquet.f@gmail.com>
2017-08-02 13:49:08 +0200

    fix test, well ... :-)

--- examples/modeling/src/test/java/test/CustomTypesTest.java
+++ examples/modeling/src/test/java/test/CustomTypesTest.java
@@ -9,9 +9,6 @@ import model.ModelPlugin;
 import model.SmartCity;
 import org.junit.Assert;
 
-/**
- * Created by Gregory NAIN on 02/08/2017.
- */
 public class CustomTypesTest {
     private static long cityNodeId;
     private static MockStorage storage = new MockStorage();
@@ -23,12 +20,12 @@ public class CustomTypesTest {
         g.connect(new Callback<Boolean>() {
             @Override
             public void on(Boolean result) {
-                SmartCity city = SmartCity.create(0,0, g);
+                SmartCity city = SmartCity.create(0, 0, g);
                 cityNodeId = city.id();
-                
+
                 GPSPosition location = city.getOrCreateLocation();
                 location.setLat(1.2);
-                location.setLat(3.4);
+                location.setLng(3.4);
                 
                 g.save((saved) -> {
 
@@ -36,7 +33,7 @@ public class CustomTypesTest {
                     graph2.connect(new Callback<Boolean>() {
                         @Override
                         public void on(Boolean result) {
-                            graph2.lookup(0,0, cityNodeId, city ->{
+                            graph2.lookup(0, 0, cityNodeId, city -> {
                                 SmartCity retreivedCity = (SmartCity) city;
                                 GPSPosition location = retreivedCity.getLocation();
                                 Assert.assertTrue(location.getLat() == 1.2);
@@ -47,8 +44,8 @@ public class CustomTypesTest {
                 });
             }
         });
-        
-        
+
+
     }
-    
+
 }
